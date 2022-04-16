ab8fc51b2b147e843977c0a264657adfd3f2d84d
Aleksey <a.luchinin@tinkoff.ru>
2022-04-16 15:55:13 +0300

    Add ProgressBars to AddCourseActivity and CourseInfoActivity and download music button

--- app/src/main/java/ru/hse/meditation/ui/course/add/AddCourseActivity.kt
+++ app/src/main/java/ru/hse/meditation/ui/course/add/AddCourseActivity.kt
@@ -2,11 +2,15 @@ package ru.hse.meditation.ui.course.add
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.*
 import ru.hse.meditation.R
-import ru.hse.meditation.model.entity.Course
+import ru.hse.meditation.model.repository.CourseRepository
 import ru.hse.meditation.ui.ActivityWithBackButton
 import ru.hse.meditation.ui.adapter.AddCourseAdapter
 import ru.hse.meditation.ui.course.create.CreateCourseActivity
@@ -22,26 +26,24 @@ class AddCourseActivity : ActivityWithBackButton() {
         val adapter = AddCourseAdapter(this)
         listOfCourses.adapter = adapter
 
-        adapter.setCourseList(
-            listOf(
-                Course(
-                    "id1",
-                    "Lesha",
-                    "",
-                    1
-                ),
-                Course(
-                    "id2",
-                    "Lesha2",
-                    "",
-                    1
-                )
-            )
-        )
-
-//        TODO("Load from github")
-
         val newCourseButton: Button = findViewById(R.id.new_course)
+
+        val progressBar: ProgressBar = findViewById(R.id.progressBarCoursesFromGithub)
+
+        lifecycleScope.launch {
+            newCourseButton.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+
+            withContext(Dispatchers.IO) {
+                val courseRepository = CourseRepository(application)
+
+                val courses = courseRepository.loadNewCourses()
+                adapter.setCourseList(courses)
+            }
+            progressBar.visibility = View.INVISIBLE
+            newCourseButton.visibility = View.VISIBLE
+        }
+
         newCourseButton.setOnClickListener {
             val intent = Intent(applicationContext, CreateCourseActivity::class.java)
             startActivity(intent)
--- app/src/main/java/ru/hse/meditation/ui/course/info/CourseInfoActivity.kt
+++ app/src/main/java/ru/hse/meditation/ui/course/info/CourseInfoActivity.kt
@@ -1,9 +1,18 @@
 package ru.hse.meditation.ui.course.info
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.hse.meditation.R
 import ru.hse.meditation.model.entity.Course
+import ru.hse.meditation.model.repository.CourseRepository
 import ru.hse.meditation.ui.ActivityWithBackButton
 
 class CourseInfoActivity : ActivityWithBackButton() {
@@ -14,7 +23,28 @@ class CourseInfoActivity : ActivityWithBackButton() {
         val course = intent.getSerializableExtra("course") as Course
 
         val textView: TextView = findViewById(R.id.info_about_course)
+        textView.text = "Description course \"${course.name}\": ${course.description}"
 
-        textView.text = "${course.name} is about ..."
+        val progressBar: ProgressBar = findViewById(R.id.progressBarMusicFromGithub)
+
+        val downloadCourseButton: Button = findViewById(R.id.download_course)
+
+        downloadCourseButton.setOnClickListener {
+            lifecycleScope.launch {
+                progressBar.visibility = View.VISIBLE
+
+                withContext(Dispatchers.IO) {
+                    withContext(NonCancellable) {
+
+                        val courseRepository = CourseRepository(application)
+                        courseRepository.insert(course)
+
+                        // TODO save music
+                    }
+                }
+                progressBar.visibility = View.INVISIBLE
+                onBackPressed()
+            }
+        }
     }
 }
