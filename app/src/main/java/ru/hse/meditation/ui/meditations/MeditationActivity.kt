d928c4c55dcd66ebffca7f57bf3afda424f9fe28
Mark Vavilov <Mark.Vavilov@jetbrains.com>
2022-04-20 01:15:12 +0300

    add notifications

--- app/src/main/java/ru/hse/meditation/ui/meditations/MeditationActivity.kt
+++ app/src/main/java/ru/hse/meditation/ui/meditations/MeditationActivity.kt
@@ -1,50 +1,105 @@
 package ru.hse.meditation.ui.meditations
 
-import android.media.MediaPlayer
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import ru.hse.meditation.R
 import ru.hse.meditation.databinding.ActivityMeditationBinding
 import ru.hse.meditation.model.entity.Practice
-import ru.hse.meditation.model.repository.MusicRepository
 import ru.hse.meditation.ui.ActivityWithBackButton
+import ru.hse.meditation.ui.notifications.MediaSessionService
+import ru.hse.meditation.ui.notifications.audioIntentForService
+
+const val audioIntent = "AudioIntent"
 
 class MeditationActivity : ActivityWithBackButton() {
-    private lateinit var mediaPlayer: MediaPlayer
+    lateinit var binding: ActivityMeditationBinding
+
+    override fun onResume() {
+        super.onResume()
+
+        val playButton: Button = findViewById(R.id.play)
+        val pauseButton: Button = findViewById(R.id.pause)
+        val stopButton: Button = findViewById(R.id.stop)
+
+        val broadcastManager = LocalBroadcastManager.getInstance(this)
+        playButton.setOnClickListener {
+            broadcastManager.sendBroadcast(Intent(audioIntentForService).also {
+                it.putExtra("switch", "play")
+            })
+        }
+        pauseButton.setOnClickListener {
+            broadcastManager.sendBroadcast(Intent(audioIntentForService).also {
+                Log.e("switch Activity", "pause")
+                it.putExtra("switch", "pause")
+            })
+        }
+
+        stopButton.setOnClickListener {
+            broadcastManager.sendBroadcast(Intent(audioIntentForService).also {
+                it.putExtra("switch", "stop")
+            })
+            onBackPressed()
+        }
+
+        broadcastManager.registerReceiver(messageReceiver, IntentFilter(audioIntent))
+    }
+
+    private val messageReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == audioIntent) {
+                val playButton: Button = findViewById(R.id.play)
+                val pauseButton: Button = findViewById(R.id.pause)
+
+                intent.getStringExtra("time")?.let { time ->
+                    binding.timer.text = time
+                }
+                intent.getStringExtra("switch")?.let { switch ->
+                    when (switch) {
+                        "play" -> {
+                            playButton.visibility = View.GONE
+                            pauseButton.visibility = View.VISIBLE
+                        }
+                        else -> {
+                            playButton.visibility = View.VISIBLE
+                            pauseButton.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver)
+        super.onPause()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMeditationBinding.inflate(layoutInflater)
+        binding = ActivityMeditationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val practice = intent.getSerializableExtra("practice") as Practice
         title = practice.name
 
-        val timer = object : CountDownTimer(practice.duration * 60 * 1000L, 1000) {
-            override fun onTick(millis: Long) {
-                val totalSeconds = millis / 1000
-                val seconds = totalSeconds % 60
-                val minutes = totalSeconds / 60
+        val serviceOn = intent.getBooleanExtra("serviceOn", false)
+        if (serviceOn) return
 
-                val text = "%02d:%02d".format(minutes, seconds)
-                binding.timer.text = text
-            }
+        val intent = Intent(applicationContext, MediaSessionService::class.java)
+        intent.putExtra("practice", practice)
 
-            override fun onFinish() {
-                onBackPressed()
-            }
-        }
-        timer.start()
-
-        val musicRepository = MusicRepository(application)
-        mediaPlayer = MediaPlayer()
-        mediaPlayer.setDataSource(musicRepository.getMusicFileFor(practice.courseId, practice.audioName).absolutePath)
-        mediaPlayer.isLooping = true
-        mediaPlayer.prepare()
-        mediaPlayer.start()
+        ContextCompat.startForegroundService(
+            applicationContext,
+            intent
+        )
     }
 
-    override fun onStop() {
-        super.onStop()
-        mediaPlayer.stop()
-    }
 }
